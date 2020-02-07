8d5c88f01bdd42c25bde4621df9835e7397b315c
SandraFloresB <sandrae.floresb@gmail.com>
2020-02-06 19:08:36 -0600

    Se agregan MarioFacil y MarioDificil

---
+++ src/src1/tareas/MarioDificilSandra.java
@@ -0,0 +1,47 @@
+package src1.tareas;
+
+import libs.Input;
+
+public class MarioDificilSandra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Indica la altura de la piramide: ");
+		int altura = Input.get_int();
+		int a = 0;
+		String caracteres = "#";
+
+		//Ciclo para imprimir renlon
+		for(int i = 0, n = altura; i < n; i++) {
+			int esp = altura - 1;
+			String espacios = "";   //inicializa los espacios para cada renglon
+				
+			   //Ciclo para agregar espacios al renglon
+				for(int ren = a; ren != altura-1; ren++ ) {
+					espacios = espacios + " ";
+				}
+				
+				//Agrega caracteres al renglon
+				caracteres = caracteres + "#";
+				a = a + 1;
+
+			//Imprime renglon	
+			System.out.println(espacios + caracteres + "  " + caracteres);
+		}
+		
+		
+	}
+
+}
+
+
+
+//Implementa un programa que muestre una piramide doble como la que se muestra abajo.
+//
+//$ ./mario
+//Altura: 4
+//   #  #
+//  ##  ##
+// ###  ###
+//####  ####
---
+++ src/src1/tareas/MarioFacilSandra.java
@@ -0,0 +1,54 @@
+package src1.tareas;
+
+import libs.Input;
+
+public class MarioFacilSandra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("Indica la altura de la piramide: ");
+		int altura = Input.get_int();
+		int a = 0;
+		String caracteres = "#";
+
+		//Ciclo para imprimir renlon
+		for(int i = 0, n = altura; i < n; i++) {
+			int esp = altura - 1;
+			String espacios = "";   //inicializa los espacios para cada renglon
+				
+			   //Ciclo para agregar espacios al renglon
+				for(int ren = a; ren != altura-1; ren++ ) {
+					espacios = espacios + " ";
+				}
+				
+				//Agrega caracteres al renglon
+				caracteres = caracteres + "#";
+				a = a + 1;
+
+			//Imprime renglon	
+			System.out.println(espacios + caracteres);
+		}
+		
+		
+	}
+}
+
+
+//
+//Implementa un programa que imprima una media piramide de una altura especificada por el usuario, como abajo:
+//
+//$ ./mario
+//Altura: 5
+//    ##
+//   ###
+//  ####
+// #####
+//######
+//
+//$ ./mario
+//Altura: 3
+//  ##
+// ###
+//####
+
